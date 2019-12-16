e52cfc992c4846ffa9a0292c61c2fe5737ce4edc
Andrés Duque <desarrollo@dexcondigital.com>
2019-12-16 14:17:31 -0500

    Revert "Merge branch 'master' of https://github.com/DexconDigital/gomezy-asociados"
    This reverts commit 726c54e4abc464ebdde0875c0b7ccc135acf488f, reversing
    changes made to d624ee23d6b3c82c4b8871c86d8d768f26f554df.

--- servicios.php
+++ servicios.php
@@ -88,7 +88,7 @@ $nombre_inmobiliaria = 'Gómez Y Asociados' ?>
                 <!-- Matenimiento -->
                 <div class="col-12 borde mt-3">
                     <div class="row align-items-center">
-                        <div class="col-6 p-4">
+                    <div class="col-6 p-4">
                             <p class="text-justify"><?php echo   $texto_servicios['mantenimientos']['parrafo']  ?> </p>
                         </div>
                         <div class="col-6 p-3">
@@ -97,7 +97,7 @@ $nombre_inmobiliaria = 'Gómez Y Asociados' ?>
                                 <span><i class="<?php echo $iconos_servicios['mantenimiento'] ?>"></i></span>
                             </div>
                         </div>
-
+                        
                     </div>
                 </div>
 
@@ -134,7 +134,7 @@ $nombre_inmobiliaria = 'Gómez Y Asociados' ?>
                 <!-- inversiones -->
                 <div class="col-12 borde  mt-3">
                     <div class="row align-items-center">
-
+                       
                         <div class="col-6  p-4">
                             <h2 class="text-center"><?php echo $texto_servicios['inversiones']['titulo']  ?> </h2>
                             <div class="col-12 mt-3  text-center">
@@ -182,97 +182,15 @@ $nombre_inmobiliaria = 'Gómez Y Asociados' ?>
     </section>
     <br>
     <br>
-    <!-- Modal -->
 
-    <section>
-        <div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Consignar Inmueble</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="modal-body">
-                            <form action="email/consignainmueble.php" class="row" method="post">
-                                <div class="form-group col-12">
-                                    <input type="text" placeholder="Nombre" name="nombre" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="mail" placeholder="Correo" name="email" class="form-control" required="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="Telefono" placeholder="Teléfono" name="telefono" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <select class="form-control select_color" name="transaccion" requiered="">
-                                        <option selected="" disabled="">Tipo de Transacción</option>
-                                        <option value="arriendo">Arriendo</option>
-                                        <option value="arriendo/venta">Arriendo/venta</option>
-                                        <option value="venta">Venta</option>
-                                        <option value="avaluos">avalúos</option>
-                                    </select>
-                                </div>
-                                <div class="form-group col-12">
-                                    <select class="form-control select_color" id="tipo_inm" name="tipo_inm" requiered="">
-                                        <option selected="" disabled="">Tipo de Inmueble</option>
-                                        <option value="Apartamento">Apartamento</option>
-                                        <option value="Casa">Casa</option>
-                                        <option value="Consultorio">Consultorio</option>
-                                        <option value="Oficina">Oficina</option>
-                                        <option value="Local">Local</option>
-                                        <option value="Bodega">Bodega</option>
-                                        <option value="Lote">Lote</option>
-                                        <option value="Finca">Finca</option>
-                                        <option value="Parqueadero">Parqueadero</option>
-                                        <option value="Edificio">Edificio</option>
-                                        <option value="Apartaestudio">Apartaestudio</option>
-                                        <option value="Hotel">Hotel</option>
-                                    </select>
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="number" placeholder="Valor" name="valor" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="number" placeholder="Área" name="area" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="text" placeholder="Ciudad" name="ciudad" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <input type="text" placeholder="Dirección" name="direccion" class="form-control" requiered="">
-                                </div>
-                                <div class="form-group col-12">
-                                    <textarea name="mensaje" placeholder="Mensajes" class="form-control" id="mensaje" rows="6" requiered=""></textarea>
-                                </div>
-                                <div class="form-group col-12">
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" requiered="">
-                                        <label class="form-check-label" for="exampleCheck1">Acepto <a class="politicas_contac" href="Politicas_de_seguridad.pdf" class="a-terminos" target="_blank">la política y tratamiento de datos</a></label>
-                                    </div>
-                                </div>
-                                <div class="form-group col-12">
-                                    <button type="submit" class="btn btn-primary btn-sm btn-block color_azul">Enviar</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn color_gris" data-dismiss="modal">Cancelar</button>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <footer>
 
         <?php include 'layout/footer.php' ?>
 
     </footer>
+
+
 </body>
 <?php include 'layout/archivosfooter.php'; ?>
 
