e42959fcb80c05d9df3837436ea5cac0fe9610b1
william solano <wdsp9898@gmail.com>
2019-12-16 07:39:40 -0500

    se realiza la implemnetacion del buscador

--- layout/buscador.php
+++ layout/buscador.php
@@ -1,48 +1,44 @@
 <div class="container p-0">
 
-            <div class="col-12">
-                <div class="row justify-content-center">
+    <div class="col-12">
+        <div class="row justify-content-center">
 
-                    <input type="text" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Código">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="codigo_buscar" placeholder="Código">
 
-                    <select style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlSelect1">
-                        <option disabled selected> Tipo de Inmueble </option>
-                        <option value=""></option>
-                    </select>
+            <select style="width:15rem" class="mx-3 my-2  form-control" id="tipo_inmueble_buscar">
+                <option selected="" value="0"> Tipo de Inmueble </option>
+            </select>
 
-                    <select style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlSelect1">
-                        <option disabled selected> Tipo de Gestión </option>
-                        <option value=""></option>
-                    </select>
+            <select style="width:15rem" class="mx-3 my-2  form-control" id="tipo_gestion_buscar">
+                <option selected="" value="0"> Tipo de Gestión </option>
+            </select>
 
-                    <select style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlSelect1">
-                        <option disabled selected> Ciudad </option>
-                        <option value=""></option>
-                    </select>
+            <select style="width:15rem" class="mx-3 my-2  form-control" id="ciudad_buscar">
+                <option selected="" value="0"> Ciudad </option>
+            </select>
 
-                    <select style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlSelect1">
-                        <option disabled selected> Barrio </option>
-                        <option value=""></option>
-                    </select>
+            <select style="width:15rem" class="mx-3 my-2  form-control" id="barrio_buscar">
+                <option selected="" value="0"> Barrio </option>
+            </select>
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Precio Mínimo">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="precio_minimo_buscar" placeholder="Precio Mínimo">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Precio Máximo">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="precio_maximo_buscar" placeholder="Precio Máximo">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Área Mínima">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="area_minima_buscar" placeholder="Área Mínima">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Área Máxima">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="area_maxima_buscar" placeholder="Área Máxima">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Baños">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="banios_buscar" placeholder="Baños">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Alcobas">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="alcobas_buscar" placeholder="Alcobas">
 
-                    <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="exampleFormControlInput1" placeholder="Garajes">
+            <input type="number" style="width:15rem" class="mx-3 my-2  form-control" id="garajes_buscar" placeholder="Garajes">
 
-                    <button type="button" style="width:15rem" class="text-capitalize mx-3 my-2  btn color_boton"> Buscar </button>
+            <button type="button" style="width:15rem" class="text-capitalize mx-3 my-2  btn color_boton" id="buscar"> Buscar </button>
 
-
-                </div>
-            </div>
 
         </div>
+    </div>
+
+</div>
