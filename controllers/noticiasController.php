c757dc13eb4ba203d65661f3720c9ea7cdb308c8
william solano <wdsp9898@gmail.com>
2020-01-23 14:21:40 -0500

    se realiza la validacion de tamaño de titulos

--- controllers/noticiasController.php
+++ controllers/noticiasController.php
@@ -40,7 +40,7 @@ function modelo_ultima_noticia($r)
         $descrip = $r[$i]['descripcion'];
         $limite_de_cadena1 = 124;
         $limite_de_cadena11 = 120;
-        $limite_de_cadena2 = 24;
+        $limite_de_cadena2 = 29;
         // recortar cadena
         //pedniente recortar cadena de descripcion corta
         if (strlen($nombre) >= $limite_de_cadena2) {
@@ -74,14 +74,30 @@ function modelo_ultima_noticia($r)
                { 
 
                    for ($i = 0; $i < count($r); $i++) {
+                    $nombre = $r[$i]['titulo'];
+                    $descrip = $r[$i]['descripcion'];
+                    $limite_de_cadena1 = 124;
+                    $limite_de_cadena11 = 120;
+                    $limite_de_cadena2 = 29;
+                      // recortar cadena
+                      //pedniente recortar cadena de descripcion corta
+                       if (strlen($nombre) >= $limite_de_cadena2) {
+                          $nombre = substr($nombre, 0, $limite_de_cadena2) . '...';
+                      }else {
+                          $nombre = $nombre . '...';
+                      }if (strlen($descrip) >= $limite_de_cadena1) {
+                          $descrip = substr($descrip, 0, $limite_de_cadena1) . '...';
+                      }else {
+                          $descrip = $descrip . '...';
+                      }
                        echo'
                     <div class="mb-5 col-12 col-md-6 col-lg-4 col-xl-4">
                     <div class="card" style="width: 100%;">
                         <img style="object-fit: cover;width: 100%;height: 255px;" src="' . $r[$i]['imagen'] . '" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h4 class="color_titulo">'.$r[$i]['titulo'].'</h4>
+                            <h4 class="color_titulo">'.$nombre.'</h4>
                             <p class="card-text">' . $r[$i]['fecha'] . '</p>
-                            <p class="card-text">'.$r[$i]['descripcion'].'</p>
+                            <p class="card-text">'.$descrip.'</p>
                             <hr>
                             <a href="detalle_noticia.php?co=' . $r[$i]['id'] . '" class="btn boton_ver_mas rounded">Ver Más</a>
                         </div>
